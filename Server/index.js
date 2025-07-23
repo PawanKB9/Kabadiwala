b6087ae2bf9956b8861b8d6a6d11053aeb3b508c
Pawan <220104041@hbtu.ac.in>
2025-07-23 22:39:07 +0530

    Initial commit

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoutes from './Router/UserRoute.js'
+import adminRoute from './Router/TempMalik.js'
+import orderRoute from './Router/SellRoute.js'
+import connectDB from './Database/ConnectDB.js';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+
+const baseUrl = process.env.BASE_URL;
+const app = express();
+
+connectDB();
+
+// CORS for frontend (React/Vite)
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(`/${baseUrl}/user`, userRoutes);
+app.use(`/${baseUrl}/admin`, adminRoute);
+app.use(`/${baseUrl}/order`, orderRoute);
+
+app.listen(5000, () => {
+  console.log('🚀 Server running on http://localhost:5000');
+});
